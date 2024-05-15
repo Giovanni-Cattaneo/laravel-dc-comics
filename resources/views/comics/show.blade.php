c5ec7f672e81b54f63b1705c47977beec8edcf94
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-15 23:11:35 +0200

    edit card dimension

--- resources/views/comics/show.blade.php
+++ resources/views/comics/show.blade.php
@@ -4,14 +4,13 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <div class="card text-start">
+                <div class="card w-25">
                     <img class="card-img-top" src="{{ $comic->thumb }}" alt="Title" />
                     <div class="card-body">
                         <h4 class="card-title">{{ $comic->title }}</h4>
                         <p class="card-text">{{ $comic->description }}</p>
                     </div>
                 </div>
-
             </div>
         </div>
     </div>
