fcd8168a53adc0700d877a04aed4aa71031a64c9
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-15 14:45:23 +0200

    add layout, add comics folder in view for index and show

--- app/Http/Controllers/ComicsModelController.php
+++ app/Http/Controllers/ComicsModelController.php
@@ -12,7 +12,7 @@ class ComicsModelController extends Controller
      */
     public function index()
     {
-        //
+        return view('comics.index');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ComicsModelController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ComicsModelController::class, 'index'])->name('home');
+
+Route::resource('/comics', ComicsModelController::class);
