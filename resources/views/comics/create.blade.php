071710433e7dbe481ad9bacaf6d7312b72dd7555
Giovanni-Cattaneo <Bogio.catta@gmail.com>
2024-05-16 10:13:09 +0200

    add container in create

--- resources/views/comics/create.blade.php
+++ resources/views/comics/create.blade.php
@@ -1,48 +1,50 @@
 @extends('layout.app')
 
 @section('main-content')
-    <form action="{{ route('comics.store') }}" method="post">
-        @csrf
+    <div class="container mt-5">
+        <form action="{{ route('comics.store') }}" method="post">
+            @csrf
 
-        <div class="mb-3">
-            <label for="" class="form-label">Title</label>
-            <input type="text" class="form-control" name="title" id="title" aria-describedby="helpId"
-                placeholder="Inserisci il Titolo del fumetto" />
-            <small id="helpId" class="form-text text-muted">Help text</small>
-        </div>
-        <div class="mb-3">
-            <label for="" class="form-label">Img</label>
-            <input type="text" class="form-control" name="thumb" id="thumb" aria-describedby="helpId"
-                placeholder="Inserisci l'immagine del fumetto" />
-            <small id="helpId" class="form-text text-muted">Help text</small>
-        </div>
-        <div class="mb-3">
-            <label for="" class="form-label">Serie</label>
-            <input type="text" class="form-control" name="series" id="series" aria-describedby="helpId"
-                placeholder="Inserisci il Titolo del fumetto" />
-            <small id="helpId" class="form-text text-muted">Help text</small>
-        </div>
-        <div class="mb-3">
-            <label for="" class="form-label">price</label>
-            <input type="text" class="form-control" name="price" id="price" aria-describedby="helpId"
-                placeholder="Inserisci il Titolo del fumetto" />
-            <small id="helpId" class="form-text text-muted">Help text</small>
-        </div>
-        <div class="mb-3">
-            <label for="" class="form-label">sale date</label>
-            <input type="text" class="form-control" name="sale_date" id="sale_date" aria-describedby="helpId"
-                placeholder="Inserisci il Titolo del fumetto" />
-            <small id="helpId" class="form-text text-muted">Help text</small>
-        </div>
-        <div class="mb-3">
-            <label for="description" class="form-label">Description</label>
-            <textarea class="form-control" name="description" id="description" rows="6"></textarea>
-        </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Title</label>
+                <input type="text" class="form-control" name="title" id="title" aria-describedby="helpId"
+                    placeholder="Inserisci il Titolo del fumetto" />
+                <small id="helpId" class="form-text text-muted">Help text</small>
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Img</label>
+                <input type="text" class="form-control" name="thumb" id="thumb" aria-describedby="helpId"
+                    placeholder="Inserisci l'immagine del fumetto" />
+                <small id="helpId" class="form-text text-muted">Help text</small>
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">Serie</label>
+                <input type="text" class="form-control" name="series" id="series" aria-describedby="helpId"
+                    placeholder="Inserisci il Titolo del fumetto" />
+                <small id="helpId" class="form-text text-muted">Help text</small>
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">price</label>
+                <input type="text" class="form-control" name="price" id="price" aria-describedby="helpId"
+                    placeholder="Inserisci il Titolo del fumetto" />
+                <small id="helpId" class="form-text text-muted">Help text</small>
+            </div>
+            <div class="mb-3">
+                <label for="" class="form-label">sale date</label>
+                <input type="text" class="form-control" name="sale_date" id="sale_date" aria-describedby="helpId"
+                    placeholder="Inserisci il Titolo del fumetto" />
+                <small id="helpId" class="form-text text-muted">Help text</small>
+            </div>
+            <div class="mb-3">
+                <label for="description" class="form-label">Description</label>
+                <textarea class="form-control" name="description" id="description" rows="6"></textarea>
+            </div>
 
-        <button type="submit" class="btn btn-primary">
-            Invia
-        </button>
+            <button type="submit" class="btn btn-primary">
+                Invia
+            </button>
 
 
-    </form>
+        </form>
+    </div>
 @endsection
